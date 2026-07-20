df41a6ca7a90cfb43efebc47b081712dc9b3b83a
Fahim36-sys <fahimhasnat32@outlook.com>
2026-07-21 02:03:22 +0600

    Co-authored-by: DasOrpon <DasOrpon@users.noreply.github.com>

--- src/server/math.server.lua
+++ src/server/math.server.lua
@@ -4,12 +4,13 @@ function add(a, b)
     return a + b
 end
 local result = add(5, 3)
-print("Sum: " .. result)
+print("Sum: " ..result)
 print("Welcome to new roblox game!")
 local obj=Workspace.Baseplate.part
-obj.Anchored=true
+obj.Anchored=false
 obj.Color=Color3.fromRGB(255, 0, 0)
 --if touched obj, color will change to blue
 obj.Touched:Connect(function(hit)
     obj.Color=Color3.fromRGB(0, 0, 255)
 end)
+print ("Fahim suck Turja's dih")
